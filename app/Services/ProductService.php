b7f39e08c404c8ff30d3bbb8f47416a9f5924705
Bilal Baraz <bilalbaraz@windowslive.com>
2024-08-30 07:47:34 +0300

    Create controllers and services

---
+++ app/Services/ProductService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+use App\Models\Product;
+
+class ProductService
+{
+    private Product $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+}
