997063156007063d00eea4bd1408a96afca3f888
Bilal Baraz <bilalbaraz@windowslive.com>
2024-08-30 07:16:25 +0300

    Add product model, migration and seeder class, token creation endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/tokens/create', function (Request $request) {
+    $user = User::query()->where('email', $request->get('email'))->first();
+
+    if (!$user || !Hash::check($request->get('password'), $user->password)) {
+        return ['success' => false, 'error' => 'Invalid credentials'];
+    }
+
+    $token = $user->createToken('personal_access_token', ['*'], Carbon::now()->addHour());
+
+    return ['token' => $token->plainTextToken];
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
